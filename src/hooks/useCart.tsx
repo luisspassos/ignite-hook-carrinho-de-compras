e93abd5a929df9968d4a5c4c1e751b0d95f66fed
luis <luis.passos013@gmail.com>
2022-02-12 10:56:23 -0300

    added product rendering on home page and add products to cart function

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,20 +23,60 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart')
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const product = await api.get(`products/${productId}`)
+      const productData: Omit<Product, 'amount'> = product.data
+
+      const stock = await api.get(`stock/${productId}`)
+      const stockData: Stock = stock.data; 
+
+      const finalProduct = {
+        ...productData,
+        amount: 1
+      }
+        
+      const findProduct = cart.some(product => product.id === productData.id)
+      if(findProduct) {
+
+        const productQuantity = cart.find(product => product.id === productId)?.amount
+
+        if(productQuantity && productQuantity < stockData.amount) {
+
+        const newProducts = cart.map(product => {
+          if(product.id === productId) {
+            return {
+              ...product,
+              amount: product.amount + 1
+            }
+          } else {
+            return product
+          }
+        })
+
+        setCart(newProducts)
+
+      } else {
+        toast.error('Quantidade solicitada fora de estoque')
+      }
+
+      } else {
+        setCart(prevState => [...prevState, finalProduct])
+      }
+
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart))
+
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto')
     }
   };
 
